44fef1d07ad48e272afbcc856670045fdcdd9cbb
mstssk <mstssk+github@gmail.com>
2021-04-08 22:32:41 +0900

    jsdocでTypeScriptの型定義を使う

---
+++ jsdoc-at-type/.eslintrc.js
@@ -0,0 +1,5 @@
+/** @type {import("@typescript-eslint/experimental-utils").TSESLint.Linter.Config} */
+const config = {
+  extends: ["foobar"],
+};
+module.exports = config;
---
+++ jsdoc-at-type/jest.config.js
@@ -0,0 +1,5 @@
+/** @type {import("@jest/types").Config.InitialOptions} */
+const config = {
+  preset: "foobar",
+};
+module.exports = config;
