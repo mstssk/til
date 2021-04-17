0613378dbeb478af648a5b2ab920e1e96dea4efb
mstssk <mstssk+github@gmail.com>
2021-04-17 22:58:09 +0900

    https://nextjs.org/learn/basics/assets-metadata-css/styling-tips までやった

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,9 +1,14 @@
 import Link from "next/link";
 import Image from "next/image";
+import Head from "next/head";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
@@ -16,6 +21,6 @@ export default function FirstPost() {
         width={144} // Desired size with correct aspect ratio
         alt="Your Name"
       />
-    </>
+    </Layout>
   );
 }
