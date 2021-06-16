6aeb6476dc33130b91e810207f273b90a3d27f4c
mstssk <mstssk+github@gmail.com>
2021-06-16 16:08:34 +0900

    half yearly

--- breejs_later/test.js
+++ breejs_later/test.js
@@ -1,7 +1,7 @@
 const later = require("@breejs/later");
 
 // https://breejs.github.io/later/parsers.html
-const sched = later.parse.text("every weekend of the month");
+const sched = later.parse.text("every 6 months on the first day of the month");
 const occurrences = later.schedule(sched).next(5);
 if (Array.isArray(occurrences)) {
   occurrences.forEach((item) => {
