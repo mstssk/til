5d37d0dcc0b74ef4855d40797f5f82eb806a75e2
mstssk <mstssk+github@gmail.com>
2021-11-01 23:27:23 +0900

    drag handle

--- react-dndkit/src/SortableItem.tsx
+++ react-dndkit/src/SortableItem.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, CSSProperties } from "react";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 
@@ -6,14 +6,22 @@ export const SortableItem: FC<{ id: string }> = (props) => {
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id: props.id });
 
-  const style = {
+  const style: CSSProperties = {
     transform: CSS.Transform.toString(transform),
     transition,
   };
 
+  const handleStyle: CSSProperties = {
+    background: "none",
+    borderColor: "transparent",
+  };
+
   return (
-    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
-      {props.children}
+    <div ref={setNodeRef} style={style}>
+      <button {...attributes} {...listeners} style={handleStyle}>
+        ☰
+      </button>
+      <span>{props.children}</span>
     </div>
   );
 };
