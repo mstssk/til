73b83040d374f2c5c093434d10078978882d1de9
mstssk <mstssk+github@gmail.com>
2022-05-02 21:20:07 +0900

    fractionalSecondDigits

---
+++ fractionalSecondDigits/src/index.ts
@@ -0,0 +1,9 @@
+Intl.DateTimeFormat("en-US", { fractionalSecondDigits: 0 });
+// console.log(
+//   Intl.DateTimeFormat("ja-JP", {
+//     minute: "2-digit",
+//     second: "2-digit",
+//     fractionalSecondDigits: 0,
+//     timeZone: "UTC",
+//   }).format(1234_500)
+// );
